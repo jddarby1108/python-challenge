145a24441310765d52695a664ee6f924c54d623a
jddarby <67566019+jddarby1108@users.noreply.github.com>
2020-08-17 21:04:56 -0500

    Commit 2 understanding row values convert to list

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,15 +1,27 @@
 import os
 
 import csv
-csvpath = os.path.join('Resources', 'budget_data.csv')
+csv_path = os.path.join('Resources', 'budget_data.csv')
 
-with open(csvpath, 'r') as csvfile:
-    csvreader = csv.reader(csvfile,delimiter=',')
 
-    reader = csv.DictReader(csvfile)
-    for row in reader:
-         print(row['Date'], row['Profit/Losses'])
+#Open and read csv
+with open(csv_path, 'r') as csv_file:
+    csv_reader = csv.reader(csv_file, delimiter=',')
 
+    #Read the header row
+    csv_header = next(csv_file)
+    print(f"Header: {csv_header}")
+
+
+    # # Read through each row of the data
+    for row in csv_reader:
+
+        date = (row[0])
+        profit_loss = (row[1])
+
+        print(date)
+        print(profit_loss)
+        
     
         
 
